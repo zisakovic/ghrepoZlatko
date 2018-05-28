3cfcd6f10e71d5f6275635611afcd0da4cb45d6a
Zlatko.Isakovic <Zlatko.Isakovic@int-vast02.intens.local>
2018-05-28 17:02:30 +0200

    p

--- GitZlatko1/src/paket1/Klasa1.java
+++ GitZlatko1/src/paket1/Klasa1.java
@@ -6,11 +6,8 @@ public class Klasa1 {
 		
 		System.out.println("poruka 1");
 		System.out.println("poruka 2");
-<<<<<<< HEAD
 		System.out.println("poruka zisak-ovic 2");
-=======
 		System.out.println("poruka zisak-ovic 1");
->>>>>>> branch 'master' of https://github.com/zisakovic/ghrepoZlatko/
 	}
 
 }
